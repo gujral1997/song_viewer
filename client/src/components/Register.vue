c66b8adc37ec4211e050d99b46e8556885e2eae7
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-08 01:00:05 +0530

    color changed to green

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,7 +2,7 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
+        <v-toolbar flat dense class="light-green" dark>
           <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
       <div class="pl-4 pr-4 pt-2 pb-2">
@@ -23,7 +23,7 @@
         <div class="error" v-html="error" />
         <br>
         <v-btn
-          class="cyan"
+          class="light-green"
           @click="register">
           Register
         </v-btn>
