6b9d1352750261d865e297de66031f3ee007a6d8
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-10 06:18:57 +0530

    header adder with register navigation

---
+++ client/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+      <v-toolbar fixed class="light-green">
+            <v-toolbar-title class="mr-4">
+                  Header
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items>
+                  <!--router-link to="register"--> <!--Look through register in index.js-->
+                        <v-btn flat dark
+                        @click="navigateTo({name: 'register'})">
+                              Sign Up
+                        </v-btn>
+                  <!--/router-link-->
+            </v-toolbar-items>
+      </v-toolbar>
+</template>
+
+<script>
+export default {
+      methods: {
+            navigateTo (route) {
+                  this.$router.push(route)
+            }
+      }
+}
+</script>
+
+<style scoped>
+</style>
