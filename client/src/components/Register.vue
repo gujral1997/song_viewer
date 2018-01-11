7d2d64bb27120cc27e570ee6b61a3880ec3c84d8
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-11 07:04:17 +0530

    register autofill removed

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -6,6 +6,9 @@
           <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
       <div class="pl-4 pr-4 pt-2 pb-2">
+        <form
+        name="tab-tracker-form"
+        autocomplete="off">
         <v-text-field
           label="E-mail"
           v-model="email">
@@ -21,7 +24,9 @@
                :append-icon-cb="() => (e1 = !e1)"
                :type="e1 ? 'password' : 'text'"
                counter
+               autocomplete="new-password"
              ></v-text-field>
+        </form>
         <br>
         <div class="error" v-html="error" />
         <br>
