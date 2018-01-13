8cbad4f8955af3622ef7ec48afafa78514b1986a
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-13 17:40:22 +0530

    songs services front end created

---
+++ client/src/components/CreateSongs.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <panel title="Song Metadata">
+      <v-text-field
+        label="Title"
+        v-model="text">
+      </v-text-field>
+
+      <v-text-field
+        label="Artist"
+        v-model="text">
+      </v-text-field>
+
+      <v-text-field
+        label="Title"
+        v-model="title">
+      </v-text-field>
+
+      <v-text-field
+        label="Genre"
+        v-model="genre">
+      </v-text-field>
+
+      <v-text-field
+        label="Album"
+        v-model="album">
+      </v-text-field>
+
+      <v-text-field
+        label="AlbumImageURL"
+        v-model="aiu">
+      </v-text-field>
+
+      <v-text-field
+        label="YoutubeId"
+        v-model="youtubeid">
+      </v-text-field>
+
+      <v-text-field
+        label="Lyrics"
+        v-model="lyrics">
+      </v-text-field>
+
+      <v-text-field
+        label="Tab"
+        v-model="tab">
+      </v-text-field>
+    </panel>
+  </div>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+
+export default {
+  data () {
+    return {
+      title: null,
+      artist: null,
+      genre: null,
+      album: null,
+      albumImageUrl: null,
+      youtubeId: null,
+      lyrics: null,
+      tab: null
+    }
+  },
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,7 +2,13 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-
+        <div
+        v-for="song in songs"
+        :key="song.id">
+          {{song.title}}
+          {{song.artist}}
+          {{song.album}}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
@@ -10,9 +16,19 @@
 
 <script>
 import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
 export default {
   components: {
     Panel // my panel then <my-panel /> instead of <panel />
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () { // if not async, mount wont work
+    // do request to the backend for all the Songs
+    this.songs = (await SongsService.index()).data // It will send data in panel
   }
 }
 </script>
