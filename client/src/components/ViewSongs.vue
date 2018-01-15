f5c6908d144c30ddf95e444c1acc2b44c74c7d13
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-15 06:53:19 +0530

    songs routed

--- client/src/components/ViewSongs.vue
+++ client/src/components/ViewSongs.vue
@@ -3,9 +3,18 @@
 </template>
 
 <script>
+import SongsService from '@/services/SongsService'
 
 export default {
-
+  data () {
+    return {
+      song: null
+    }
+  },
+  async mounted () {
+    const songId = this.$store.state.route.params.songId
+    const song = await SongsService.show(songId)
+  }
 }
 </script>
 
