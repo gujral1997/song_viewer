551453822a5884de94975cc7f63ff5761f092c83
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-17 08:24:04 +0530

    song panel seperated

---
+++ client/src/components/Songs/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <songs-panel />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+import SongsService from '@/services/SongsService'
+import SongsPanel from './SongsPanel'
+export default {
+  components: {
+    Panel, // my panel then <my-panel /> instead of <panel />
+    SongsPanel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () { // if not async, mount wont work
+    // do request to the backend for all the Songs
+    this.songs = (await SongsService.index()).data // It will send data in panel
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
