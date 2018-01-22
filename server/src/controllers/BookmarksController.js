4f9722e7dbd4df0c23098c8bfad4bc1bab7af2ab
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-22 14:51:25 +0530

    persisted storage to avoid logout on reload

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -17,5 +17,31 @@ module.exports = {
         error: 'An error has occured, trying to fetch bookmark'
       })
     }
+  },
+  async post (req, res) {
+    try {
+      const bookmark = req.body
+
+      await Bookmark.create(bookmark)
+      res.send(bookmark)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured, trying to create bookmark'
+      })
+    }
+  },
+  async delete (req, res) {
+    try {
+      const {bookmarkId} = req.params
+
+      const bookmark = await Bookmark.findById(bookmarkId
+      )
+      await bookmark.destroy()
+      res.send(bookmark)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured, trying to delete bookmark'
+      })
+    }
   }
 }
