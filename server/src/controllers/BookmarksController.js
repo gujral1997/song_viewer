d000c97a5bd531d2b55418e515382c519c423a5e
Ansh Gujral <anshgujral1997@gmail.com>
2018-01-24 16:57:11 +0530

    bookmark can be added only once

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -20,9 +20,20 @@ module.exports = {
   },
   async post (req, res) {
     try {
-      const bookmark = req.body
+      const {songId, userId} = req.body
+      const bookmark = await Bookmark.findOne({
+        where: {
+          SongId: songId,
+          UserId: userId
+        }
+      })
+      if (bookmark) {
+        return res.status(400).send({
+          error: 'already a bookmark'
+        })
+      }
 
-      await Bookmark.create(bookmark)
+      const newBookmark = await Bookmark.create(bookmark)
       res.send(bookmark)
     } catch (err) {
       res.status(500).send({
